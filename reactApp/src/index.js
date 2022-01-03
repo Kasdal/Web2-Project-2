85bca883488b887a5209962d166480c633874db9
kasdal <plesmilan@hotmail.com>
2022-01-03 16:05:23 +0000

    Reworked folder structure added some components from previous assignement and added private routes. Need to rework a bit

--- reactApp/src/index.js
+++ reactApp/src/index.js
@@ -1,47 +1,60 @@
 import React from "react";
-  import ReactDOM from "react-dom";
-  import { BrowserRouter, Route, Redirect, Switch, Link } from "react-router-dom";
-  import { PublicPage, Movies, Profile, HomePage } from "./pages";
-  import LoginPage from "./loginPage";
-  import AuthProvider from "./authContext";
-  import PrivateRoute from "./privateRoute";
-  import AuthHeader from "./authHeader";
-  import SignUpPage from "./signUpPage";
-  import MovieProvider from "./moviesContext";
+import ReactDOM from "react-dom";
+import {BrowserRouter, Route, Redirect, Switch} from "react-router-dom";
+import HomePage from "./pages/homePage";
+import MoviePage from "./pages/movieDetailsPage";
+import FavoriteMoviesPage from "./pages/favoriteMoviesPage";
+import MovieReviewPage from "./pages/movieReviewPage";
+import SiteHeader from './components/siteHeader'
+import UpcomingMoviesPage from "./pages/upcomingMoviesPage";
+import {QueryClientProvider, QueryClient} from "react-query";
+import {ReactQueryDevtools} from 'react-query/devtools'
+import MoviesContextProvider from "./contexts/moviesContext";
+import AddMovieReviewPage from './pages/addMovieReviewPage'
+import TrendingMoviesPage from "./pages/trendingMoviesPage";
+import NowPlayingPage from "./pages/nowPlayingPage";
+import MustWatchPage from "./pages/mustWatchPage";
+import LoginPage from "./pages/loginPage";
+import SignUpPage from "./pages/signUpPage";
+import PrivateRoute from "./components/privateRoute";
+import AuthContextProvider from "./contexts/authContext";
 
-  const App = () => {
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            staleTime: 360000,
+            refetchInterval: 360000,
+            refetchOnWindowFocus: false
+        },
+    },
+});
+
+const App = () => {
     return (
-      <BrowserRouter>
-        <AuthProvider>
-          <AuthHeader />
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/public">Public</Link>
-            </li>
-            <li>
-              <Link to="/movies">Movies</Link>
-            </li>
-            <li>
-              <Link to="/profile">Profile</Link>
-            </li>
-          </ul>
-          <MovieProvider>
-          <Switch>
-            <Route path="/signup" component={SignUpPage} />
-            <Route path="/public" component={PublicPage} />
-            <Route exact path="/" component={HomePage} />
-            <Route path="/login" component={LoginPage} />
-            <PrivateRoute path="/movies" component={Movies} />
-            <PrivateRoute path="/profile" component={Profile} />
-            <Redirect from="*" to="/" />
-          </Switch>
-          </MovieProvider>
-        </AuthProvider>
-      </BrowserRouter>
+        <QueryClientProvider client={queryClient}>
+            <BrowserRouter>
+                <SiteHeader/>
+                <MoviesContextProvider>
+                    {" "}
+                    <Switch>
+                        <Route path="/login" componenet={LoginPage}/>
+                        <Route exact path="/reviews/form" component={AddMovieReviewPage}/>
+                        <Route exact path="/movies/upcoming" component={UpcomingMoviesPage}/>
+                        <Route exact path="/movies/trending" component={TrendingMoviesPage}/>
+                        <Route exact path="/reviews/:id" component={MovieReviewPage}/>
+                        <PrivateRoute exact path="/movies/favorites" component={FavoriteMoviesPage}/>
+                        <PrivateRoute exact path="/movies/must-watch" component={MustWatchPage}/>
+                        <Route exact path="/movies/now-playing" component={NowPlayingPage}/>
+                        <Route exact path="/movies/:id" component={MoviePage}/>
+                        <Route exact path="/" component={HomePage}/>
+                        <Route path="/signup" component={SignUpPage}/>
+                        <Redirect from="*" to="/"/>
+                    </Switch>
+                </MoviesContextProvider>
+            </BrowserRouter>
+            <ReactQueryDevtools initialIsOpen={false}/>
+        </QueryClientProvider>
     );
-  };
+};
 
-  ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App/>, document.getElementById("root"));
